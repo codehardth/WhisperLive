093a47ca682910f94c59a2b58e2747d05ed87116
makaveli10 <vineet.suryan@collabora.com>
2024-01-09 18:10:17 +0000

    add: tensorrt backend to server

--- run_server.py
+++ run_server.py
@@ -3,36 +3,21 @@ from whisper_live.server import TranscriptionServer
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', '-p',
-                        type=int,
-                        default=9090,
-                        help="Websocket port to run the server on.")
-    parser.add_argument('--backend', '-b',
+    parser.add_argument('--backend', type=str, default='tensorrt', help='Backends from ["tensorrt", "faster_whisper"]')
+    parser.add_argument('--whisper_tensorrt_path',
                         type=str,
-                        default='faster_whisper',
-                        help='Backends from ["tensorrt", "faster_whisper"]')
-    parser.add_argument('--faster_whisper_custom_model_path', '-fw',
-                        type=str, default=None,
-                        help="Custom Faster Whisper Model")
-    parser.add_argument('--trt_model_path', '-trt',
-                        type=str,
-                        default=None,
+                        default="/root/TensorRT-LLM/examples/whisper/whisper_small_en",
                         help='Whisper TensorRT model path')
-    parser.add_argument('--trt_multilingual', '-m',
-                        action="store_true",
-                        help='Boolean only for TensorRT model. True if multilingual.')
     args = parser.parse_args()
 
     if args.backend == "tensorrt":
-        if args.trt_model_path is None:
+        if args.whisper_tensorrt_path is None:
             raise ValueError("Please Provide a valid tensorrt model path")
+        
+        from whisper_live.server_tensorrt import TranscriptionServerTRT
 
-    server = TranscriptionServer()
-    server.run(
-        "0.0.0.0",
-        port=args.port,
-        backend=args.backend,
-        faster_whisper_custom_model_path=args.faster_whisper_custom_model_path,
-        whisper_tensorrt_path=args.trt_model_path,
-        trt_multilingual=args.trt_multilingual
-    )
+        server = TranscriptionServerTRT()
+        server.run("0.0.0.0", port=6006, whisper_tensorrt_path=args.whisper_tensorrt_path)
+    else:
+        server = TranscriptionServer()
+        server.run("0.0.0.0")
