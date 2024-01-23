615cfd75e6ccf40af6d2c1761a52f90304f3d496
Chanvut.B <deszolate@icloud.com>
2024-01-23 12:16:53 +0700

    Added support for CUDA-enabled gpu for transcription server

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget https://developer.download.nvidia.com/compute/cuda/11.4.4/local_installers/cuda_11.4.4_470.82.01_linux.run
+sh cuda_11.4.4_470.82.01_linux.run
+chmod +x cuda_11.4.4_470.82.01_linux.run
