60521de8931a6cefd1f52bbac44af2d73be1d1ed
Rungwiroon Komalitipong <rungwiroon.k@codehard.co.th>
2024-03-07 04:07:42 +0000

    Merged PR 8647: Add demo web
    Add demo web

--- dotnet/TranscriptionClient/Runner/Program.cs
+++ dotnet/TranscriptionClient/Runner/Program.cs
@@ -1,17 +1,21 @@
 ﻿using System.Reactive.Linq;
+using Transcriptor.Py.Wrapper.Enums;
 using Transcriptor.Py.Wrapper.Implementation;
 
 class Program
 {
     public static async Task Main(string[] args)
     {
-        var serviceUri = new Uri("ws://localhost:8765");
+        var serviceUri = new Uri("ws://192.168.0.98:8765");
         using var transcriptor = new WhisperTranscriptor(serviceUri);
 
         var devices = await transcriptor.GetInputInterfacesAsync().ToListAsync();
-        var pulseDevice = devices.Single(d => d.Name == "default");
-
-        await transcriptor.StartRecordAsync(pulseDevice.Index);
+        var pulseDevice = devices.Single(d => d.Name == "MacBook Pro Microphone");
+        var options = new WhisperTranscriptorOptions(
+            ModelType.WhisperX, ModelSize: "large-v2", Language: "th", IsMultiLanguage: false);
+        var url = new Uri(
+            "https://cdn-live.tpchannel.org/v1/0180e10a4a7809df73070d7d8760/0180e10adac40b8ed59433d5f3ce/main.m3u8");
+        await transcriptor.StartRecordAsync(url, options);
 
         transcriptor
             .Select(m => m.Text)
